3eccdb58a7096d99d96e51ab3905a286e108136e
Matias Bustos <matiasrbustos@gmail.com>
2025-10-16 00:00:02 -0300

    feat: Configuración para GitHub Pages y script de despliegue

--- v1.3/vite.config.js
+++ v1.3/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Configuración para GitHub Pages
+  // Reemplaza 'Pre-entrega-react' con el nombre de tu repositorio
+  base: '/Pre-entrega-react/',
 })
